90db4f6c289d55609cde976e8541ca04174a0cae
zohoFrank <zuoh14@mails.tsinghua.edu.cn>
2016-05-30 21:11:12 +0800

    n-fix: attach text bugs

--- starter/mail_pic.py
+++ starter/mail_pic.py
@@ -8,23 +8,30 @@ class MailSender(object):
     """
     Attributes:
         _from_address (str): Sender's mail address
-        _to_address (List[str]): Receivers' mail addresses
+        _to_address (list[str]): Receivers' mail addresses
         __password (str): Password of sender's email
+
         _smtp (str): Sender's mail SMTP server address and port
+        _message (MIMEMultipart): MIME message
         _subject (str): Title of the mail, usually default
+        _text (str): Raw text of mail context
+        _image (list[str]): Paths of all pictures
         _debug (bool): Debug level
     """
+
     def __init__(self, sender='', receivers=None, password=''):
         if receivers is None:
             receivers = []
         self._from_address = sender
         self._to_address = receivers
         self.__password = password
+
         self._smtp = ''
-        self._subject = '[EMERGENT] Wechat Login QR code'
+        self._message = MIMEMultipart()
+        self._subject = 'Just an email'
+        self._text = ''
+        self._image = []
         self._debug = False
-        # call helpers
-        self._set_msg()
 
     # getter and setter #
     @property
@@ -34,6 +41,7 @@ class MailSender(object):
     @sender.setter
     def sender(self, email):
         self._from_address = email
+        self._set_msg()
 
     @property
     def receivers(self):
@@ -76,52 +84,57 @@ class MailSender(object):
 
     @subject.setter
     def subject(self, _subject):
-        self._subject = _subject;
+        self._subject = _subject
+
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, txt):
+        self._text = txt
 
     # helpers #
     def _set_msg(self):
-        self._message = MIMEMultipart()
         self._message['subject'] = self._subject
         self._message['From'] = self._from_address
         self._message['To'] = ','.join(self._to_address)
+
+    def _attach_all(self):
+        for path in self._image:
+            img = open(path, 'rb')
+            msg_img = MIMEImage(img.read())
+            img.close()
+            msg_img.add_header('Content-ID', '<' + path + '>')
+            rich_text = MIMEText('<img src="cid:' + path + '">', 'html', 'utf-8')
+            self._message.attach(rich_text)  # KEY HTML text must be attached before image
+            self._message.attach(msg_img)
 
     # necessary methods #
     def set_smtp(self, smtp):
         self.smtp = smtp
 
-    def attach_rich_text(self, text_part):
-        # todo Reveal image in the mail
-        """
-        Attach html format text to the mail.
-        Args:
-            text_part (str): Text with or without html elements.
-        """
-        msg_text = MIMEText('<img src="cid:qr">' + text_part, 'html', 'utf-8')
-        self._message.attach(msg_text)
-
-    def attach_img(self, path):
-        # todo Allow more pictures to be attched
-        """
-        Attach image to the mail if necessary.
-        Args:
-            path (str): Relative path of the picture file.
-        """
-        img = open(path, 'rb')
-        msg_img = MIMEImage(img.read())
-        img.close()
-        msg_img.add_header('Content-ID', '<qr>')
-        self._message.attach(msg_img)
-
     def send_mails(self):
+        self._set_msg()
+        self._attach_all()
         server = smtplib.SMTP(self._smtp)
-        server.login(self._from_address, self.__password)
         server.set_debuglevel(self._debug)
+        server.login(self._from_address, self.__password)
         try:
             server.sendmail(self._from_address, self._to_address, self._message.as_string())
         finally:
             server.quit()
 
     # optional methods #
+    def attach_img(self, path):
+        # todo Allow more pictures to be attached
+        """
+        Push all images' paths into the list
+        Args:
+            path (str): Relative path of the picture file.
+        """
+        self._image.append(path)
+
     def append_rcver(self, receiver):
         self._to_address.append(receiver)
 
@@ -131,6 +144,7 @@ class MailSender(object):
 
 if __name__ == '__main__':
     import re
+
     ms = MailSender()
     ms.sender = str(raw_input("Send from: "))
     # handling receivers
@@ -144,8 +158,9 @@ if __name__ == '__main__':
     ms.set_smtp('smtp.163.com:25')
 
     ms.subject = str(raw_input("Subject: "))
-    ms.attach_rich_text(str(raw_input("Content: ")))
 
     # todo Handling image attachment
     ms.attach_img('../Picture1.jpg')
+
+    ms.set_debug(True)
     ms.send_mails()
