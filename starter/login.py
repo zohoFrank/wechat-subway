768350d704f4ae4f3c3ad97ae54ed13ac5ba4963
zohoFrank <zuoh14@mails.tsinghua.edu.cn>
2016-06-01 00:00:43 +0800

    fix: Cancel running as main function

--- starter/login.py
+++ starter/login.py
@@ -1,52 +1,70 @@
 from mail_pic import MailSender
-
+import os
 import itchat
-import thread
-import re
+import threading
 from PIL import Image
 
 TEST_MAILBOX = 'zohotestcode@163.com'
 SMTP_SERVER = 'smtp.163.com:25'
-CONTENT = 'QR CODE'
+CONTENT = '[EMG] QR CODE from login.py'
 PASSWORD = 'zoho_test'
 
+PIC_NAME = 'QR.jpg'
 
-def send_qr():
-    ms = MailSender()
 
-    ms.sender = TEST_MAILBOX
-    ms.receivers = TEST_MAILBOX
-    ms.password = PASSWORD
-    ms.set_smtp(SMTP_SERVER)
-
-    ms.subject = CONTENT
-    ms.attach_rich_text(CONTENT)
-
+def search_qr():
+    delete_exist()
     while True:
         try:
-            img = Image.open('QR.jpg')
+            img = Image.open(PIC_NAME)
             if img is not None:
-                print "\n>> QR Found\n"
-                ms.attach_img('QR.jpg')
-                ms.send_mails()
+                print '\n>> Found QR\n>> Sending mails\n'
+                send_mail()
                 return
-        except:
-            pass
+        except Exception as err:
+            print err
 
 
-def login_method(method=''):
-    """
-    Main login functions caller.
-    Args:
-        method (str): Any string not equal to '' will be considered as using email.
-
-    """
-    thread.start_new_thread(itchat.auto_login(), ())
-    if method != '':
-        thread.start_new_thread(send_qr(), ())
-    else:
-        return
+def delete_exist():
+    cur_dir = os.getcwd()
+    pic_path = os.path.join(cur_dir, PIC_NAME)
+    if os.path.exists(pic_path):
+        os.remove(pic_path)
 
 
-if __name__ == '__main__':
-    itchat.auto_login()
+def send_mail():
+    ms = MailSender(TEST_MAILBOX, [TEST_MAILBOX], PASSWORD)
+    ms.subject = CONTENT
+    ms.smtp = SMTP_SERVER
+    ms.attach_img(PIC_NAME)
+    ms.set_debug(True)
+    ms.send_mails()
+    print ">> Mail sent!"
+
+
+class MailLogin(threading.Thread):
+    def __init__(self, func, args=()):
+        """
+        Args:
+            func (function): Function that ready to be invoked
+            args (tuple): Arguments of invoked function
+        """
+        super(MailLogin, self).__init__()
+        self.function = func
+        self.args = args
+
+    def run(self):
+        if self.args == ():
+            self.function()
+        else:
+            self.function(self.args)
+
+
+def mail_login():
+    # fixme Need thread lock
+    thread1 = MailLogin(search_qr)
+    thread2 = MailLogin(itchat.auto_login)
+    thread1.start()
+    thread2.start()
+
+mail_login()
